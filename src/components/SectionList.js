b4f1260019f59c7be2c0cab22b4e1b02a2b10f82
Dan Lopez <dan.lopez@practice.xyz>
2017-08-21 15:54:06 -0400

    Adds Mock Data

--- src/components/SectionList.js
+++ src/components/SectionList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './SectionList.css'
 
+// import sections from './SectionList.mockData';
+
 class SectionList extends Component {
   render() {
     return (
